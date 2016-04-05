a7fe776370ffb2a4bb468e039d10aa75b97fff38
System Administrator <root@Stephen-Sheridans-MacBook-Air.local>
2016-04-05 16:14:09 +0100

    Added DB methods with list and update

--- DBExample1.java
+++ DBExample1.java
@@ -3,32 +3,101 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
-public class DBExample1 {
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+ public class DBExample1 {
+	public static Connection conn;
+	
+	// This methods wraps up all the code for making a database connection
+	public static void getDBConnection(){
 		try{
 			//Register the Driver
 			Class.forName("com.mysql.jdbc.Driver");
 			//Get a connection to the database
-			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:8889/employee","root", "root");
-			//Create a statement 
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:8889/employee","root", "root");
+			print("");
+			print("Database connection successful.");
+		}
+		catch(Exception e){
+			// something went wrong with DB connection
+			print("Database Connection Error!");
+			e.printStackTrace();
+		}
+	}
+
+	// This method wraps up all the code for closing a database connection
+	public static void closeDBConnection(){
+		if (conn != null){
+			try{
+				conn.close();
+				print("");
+				print("Database closed successfully.");
+			}
+			catch(Exception e){
+				print("Error closing the DB");
+			}
+		}
+	}
+	
+	// Execute a select statement to return all the employee information
+	public static void listEmployees(){
+		try{
+			// Create a statement object
 			Statement stmt = conn.createStatement();
 			//Execute a Query
 			ResultSet rs = stmt.executeQuery("select * from emp");
 			//Process the results set
+			print("");
+			print("*** Employees ***");
 			while (rs.next()){
-				System.out.println(rs.getInt("empno") + "," + rs.getString("empname"));
-				
+				String sout = rs.getInt("empno") + "," + rs.getString("empname") + "," + 
+			                  rs.getString("empdob") + "," +  rs.getFloat("empsalary"); 
+				print(sout);
 			}
-		//clean up the enviornment	
-		rs.close();
-	    stmt.close();
-	    conn.close();
+			rs.close();
+		    stmt.close();
 		}
-		
 		catch(Exception e){
-			
-			e.printStackTrace();
+			print("Error executing SELECT statemennt");
 		}
 	}
+	
+	public static void updateEmpSalary(int empno, float amount){
+		try{
+			// Create a statement object
+			Statement stmt = conn.createStatement();
+			//Execute a Query
+			String query = "UPDATE emp SET empsalary=" + amount + " WHERE empno=" + empno + ";";
+			stmt.executeUpdate(query);
+			stmt.close();
+			print("");
+			print("Employee successfully updated.");
+		}
+		catch(Exception e){
+			print("Error executing UPDATE statemennt");
+		}
+	}
+	
+	public static void print(String s){System.out.println(s);}
+	
+	public static void main(String[] args) {
+		
+		// Make a MySql database connection
+		getDBConnection();
+		
+		// List all employees from DB
+		listEmployees();
+		
+		
+		print("");
+		print("Give Employee No. 1 a new salary");
+		// Update the salary of employee no. 1
+		updateEmpSalary(1, 200000.65f);
+		
+		// List all employees from DB
+		listEmployees();
+				
+		// Tidy up
+		closeDBConnection();
+		
+	}
+	
 }
